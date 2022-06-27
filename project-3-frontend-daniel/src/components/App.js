1f9725e916483ea6084018761bf64e5eb540849b
duniel12 <duniel12@gmail.com>
2022-06-26 18:32:56 -0600

    First commit front end react structure defined

---
+++ project-3-frontend-daniel/src/components/App.js
@@ -0,0 +1,16 @@
+import logo from '../logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to project 3 front end from Daniel!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
